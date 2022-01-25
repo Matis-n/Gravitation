cd93f393fe1d3d239436b2672f2b402452ba3248
Matis Nanni <matis@8octobre.net>
2022-01-25 11:22:29 +0100

    class boite + class Point

---
+++ gravitation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include "gravitation.hpp"
+//=================================================================================
+//                            class Point
+//=================================================================================
+Point& Point::operator+=(const Point& p)
+{ x+=p.x;y+=p.y;return *this;}
+Point& Point::operator-=(const Point& p)
+{ x-=p.x;y-=p.y;return *this;}
+Point& Point::operator*=(double a)
+{ x*=a;y*=a;return *this;}
+Point operator+(const Point& p1, const Point& p2)
+{ Point p=p1; return p+=p2;}
+Point operator-(const Point& p1, const Point& p2)
+{ Point p=p1; return p-=p2;}
+Point operator*(double a, const Point& p)
+{Point q=p; return q*=a;}
+double operator*(const Point& p1, const Point& p2)  // 2D cross product
+{ return (p1.x*p2.y-p2.x*p1.y);}
+double operator|(const Point& p1, const Point& p2)
+{ return (p1.x*p2.x+p1.y*p2.y);}
+double norme(const Point& p)
+{ return sqrt(p|p);}
+
+ostream& operator<<(ostream& out, const Point& p)
+{ out<<"("<<p.x<<","<<p.y<<")"; return out;}
--- gravitation.hpp
+++ gravitation.hpp
@@ -1,13 +1,39 @@
 #ifndef GRAVITATION_HPP
 #define GRAVITATION_HPP
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-class boite {
-    public :
-
-    private :
-    
+//=================================================================================
+//                        class Point
+//=================================================================================
+class Point
+{ public:
+    double x,y;  //2D Point coordinates
+    Point(double xi=0., double yi=0.)  : x(xi),y(yi){}
+	Point& operator+=(const Point& p);
+    Point& operator-=(const Point& p);
+    Point& operator*=(double a);
 };
+Point operator+(const Point& p1, const Point& p2);
+Point operator-(const Point& p1, const Point& p2);
+Point operator*(double a, const Point& p);
+double operator*(const Point& p1, const Point& p2); // produit vectoriel 2D
+double operator|(const Point& p1, const Point& p2); // produit scalaire  2D
+double norme(const Point& p);                       // norme euclidienne
+ostream& operator<<(ostream& out, const Point& p);
 
-
+//=================================================================================
+//                        class Boite
+//=================================================================================
+class Boite {
+    public :
+        int level; //niveau
+        Point center; //centre de la boîte
+        Point center_mass; //centre de masse
+        double mass;       //masse
+        Point& particule; //pointeur vers une liste de particule
+        Boite& fille; //pointeur vers la première boîte fille
+        Boite& soeur;//pointeur vers la boîte soeur
+};
 #endif
